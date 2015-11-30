13daf19d38f273f14a6feffa8ed285aed44488bb
pauZc <zclaurap@gmail.com>
2015-11-30 16:14:45 -0600

    version

---
+++ LinQLibrary/ClassData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinQLibrary
+{
+    class ClassData
+    {
+        DataClassesDataContext dcdc=new DataClassesDataContext();
+        public bool 
+    }
+}
